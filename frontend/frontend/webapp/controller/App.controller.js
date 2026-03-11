28c3b502ee580cd4699cfd99e7742d8fa0b3b03b
IvayloStaykov05 <istaykov10@gmail.com>
2026-03-11 22:57:48 +0200

    Add SAP UI5 frontend

---
+++ frontend/frontend/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.example.project.frontend.frontend.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ frontend/frontend/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/example/project/frontend/frontend/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
